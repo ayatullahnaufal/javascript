dfa20f65baa0e0e1e49a60ba4a721be4c58d1f7d
Ayatullah Naufal <mpannaufal@gmail.com>
2023-06-21 16:51:24 +0700

    Tugas 2 - Niomic

---
+++ tugas2.js
@@ -0,0 +1,18 @@
+var number = 30;
+var string = "Saya sebagai UX Designer";
+
+var tinggi = 174;
+if(tinggi < 174)
+tinggi = true;
+else 
+tinggi = false;
+
+var UIUX = null
+
+var prodesign;
+
+console.log(number);
+console.log(string);
+console.log(tinggi);
+console.log(UIUX);
+console.log(prodesign);
